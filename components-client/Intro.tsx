45df18f11c5ebf33cbdabb7634f0db9c7a1ff82b
PAnanasik <ilovezhenya228@gmail.com>
2023-10-25 10:35:14 +0300

    deploy

--- components-client/Intro.tsx
+++ components-client/Intro.tsx
@@ -20,19 +20,13 @@ const Intro = () => {
         <h2 className="font-semibold ss:text-[48px] xs:text-[32px] text-[28px] sm:leading-[75px] ss:leading-[60px] leading-[40px]">
           <span className="text-primary">Экономим</span> на ремонте с бонусами{" "}
           <span className="text-primary">RemontPlay</span>
-          {/* Тратим <span className="text-primary">намного меньше</span> на ремонт
-          вместе! */}
         </h2>
         <p className="mt-[15px] sm:text-[24px] text-[18px]">
           Завершите ваш ремонт в срок с помощью выгодных предложений от наших
           магазинов-партнеров
-          {/* Покупайте у наших партнёров и экономьте до 10% с каждой покупки */}
         </p>
         <p className="mt-[15px] sm:text-[24px] text-[18px]">
-          Получайте до 10% процентов с каждой покупки
-          {/* Покупайте у наших партнёров и экономьте до 10% с каждой покупки
-
-           */}
+          Получайте до 15% процентов с каждой покупки
         </p>
         <div className="mt-[45px] flex sm:flex-row flex-col items-center gap-[20px]">
           <a
@@ -58,28 +52,16 @@ const Intro = () => {
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
-        className="h-full flex items-center md:mt-[120px] mt-[20px]"
+        className="h-full w-1/2 lg:flex hidden items-center md:mt-[120px] mt-[20px]"
       >
-        <div className="relative w-full h-full lg:block hidden">
-          <Image
-            className="w-full h-full intro-animation relative"
-            src="/rounded-text.svg"
-            alt="Relationship image"
-            width={790}
-            height={700}
-            priority
-          />
-          <div className="w-[450px] h-[450px] bg-primary bg-opacity-[0.1] rounded-full absolute child flex items-center justify-center">
-            <Image
-              className="w-full h-full absolute object-cover rounded-full"
-              src="/client.svg"
-              alt="Relationship image"
-              width={450}
-              height={450}
-              priority
-            />
-          </div>
-        </div>
+        <Image
+          className="w-full h-full"
+          src="/intro-img.svg"
+          alt="Relationship image"
+          width={450}
+          height={450}
+          priority
+        />
       </motion.div>
     </div>
   );
