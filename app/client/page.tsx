faeae8714b1baa7481227d674b30a606418af90d
PAnanasik <ilovezhenya228@gmail.com>
2023-10-26 18:55:47 +0300

    deploy fixes

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -2,7 +2,7 @@ import HowWorks from "@/components-client/HowWorks";
 import Intro from "@/components-client/Intro";
 import Navbar from "@/components-client/Navbar";
 import RunningString from "@/components-client/RunningString";
-import Partners from "@/components-client/Partners";
+// import Partners from "@/components-client/Partners";
 import Staff from "@/components-client/Staff";
 import Faq from "@/components-client/Faq";
 import Footer from "@/components-client/Footer";
